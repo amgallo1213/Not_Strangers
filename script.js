23e9b9ace79d08483b18af9ecedf42d0e5ccee30
Andrea <90292543+amgallo1213@users.noreply.github.com>
2025-08-22 00:06:11 -0400

    updating script and data.json files

--- script.js
+++ script.js
@@ -136,359 +136,13 @@ const lighterPerfumes = [
 ]
 
 
-const heavierPerfumes = [
-    {
-        id: 838487,
-        quantity: 1,
-        name: "Awake Before Dawn",
-        img: "",
-        category: "heavier",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 405233,
-        quantity: 1,
-        name: "Humidity Extinguisher",
-        img: "",
-        category: "heavier",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 725037,
-        quantity: 1,
-        name: "A Complex Proposition",
-        img: "",
-        category: "heavier",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 278997,
-        quantity: 1,
-        name: "Scrumptus Precision",
-        img: "",
-        category: "heavier",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 43641,
-        quantity: 1,
-        name: "Happy Indeed",
-        img: "",
-        category: "heavier",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 295944,
-        quantity: 1,
-        name: "The Little Thief",
-        img: "",
-        category: "heavier",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 374871,
-        quantity: 1,
-        name: "Hungry Water",
-        img: "",
-        category: "heavier",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 715360,
-        quantity: 1,
-        name: "All the Swans",
-        img: "",
-        category: "heavier",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    }
-]
+
+    
 
 
 
 
 // ********************* DATA ************************************
-
-
-const allPerfumes = [
-    {
-        id: 324544,
-        quantity: 1,
-        name: "All Inclusive Vibe",
-        category: "lighter",
-        bestSeller: true,
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 521688,
-        quantity: 1,
-        name: "Beach Witch",
-        category: "lighter",
-        bestSeller: true,
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "salt, orange", heart: "ylang-ylang", base: "sandalwood" }
-        ],
-        price: 120,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 405233,
-        quantity: 1,
-        name: "Humidity Extinguisher",
-        category: "heavier",
-        bestSeller: true,
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 789489,
-        quantity: 1,
-        name: "Shogun Assassin",
-        category: "lighter",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 295944,
-        quantity: 1,
-        name: "The Little Thief",
-        category: "heavier",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 115786,
-        quantity: 1,
-        name: "Ibn Sina",
-        category: "lighter",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 545861,
-        quantity: 1,
-        name: "Crashing Sexual Traffic",
-        category: "lighter",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 623088,
-        quantity: 1,
-        name: "Abandoned Saints",
-        category: "lighter",
-        bestSeller: true,
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 909261,
-        quantity: 1,
-        name: "Stevie Nick's Piano Tuner",
-        category: "lighter",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 234513,
-        quantity: 1,
-        name: "Lukewarm Champagne",
-        category: "lighter",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 838487,
-        quantity: 1,
-        name: "Awake Before Dawn",
-        category: "heavier",
-        img: "",
-        keyWords: "",
-        notes: [
-            { "head": "", "heart": "", "base": "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 725037,
-        quantity: 1,
-        name: "A Complex Proposition",
-        category: "heavier",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 278997,
-        quantity: 1,
-        name: "Scrumptus Precision",
-        category: "heavier",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 43641,
-        quantity: 1,
-        name: "Happy Indeed",
-        category: "heavier",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 374871,
-        quantity: 1,
-        name: "Hungry Water",
-        category: "heavier",
-        img: "",
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    },
-    {
-        id: 715360,
-        quantity: 1,
-        name: "All the Swans",
-        category: "heavier",
-        img: "",
-        bestSeller: true,
-        keyWords: "",
-        notes: [
-            { head: "", heart: "", base: "" }
-        ],
-        price: 150,
-        size: 30,
-        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
-    }
-]
-
 
 
 async function fetchData() {
@@ -501,12 +155,56 @@ async function fetchData() {
 fetchData();
 
 function displayProducts(products) {
+
     const bestSellers = document.querySelector(".best-sellers-card-grid-wrapper")
-    // const list = document.querySelector(".test-list")
     products
     .map((product) => {
-        const { name, id, price} = product
+        const { name, id, price } = product
+        if ( product.bestSeller === true)
         bestSellers.innerHTML += 
+
+            `<div class="card" >
+                    <div class="card-img-container" >
+                        <img src="images/venn.jpg" alt="" class="image">
+                    </div>
+                    <div class="card-subgrid">
+                        <h4 class="card-name">${name}</h4>
+                        <p>Eau du Parfum | 50mL </p>
+                        <p class="price-span">$${price}</p>
+                        <button class="card-btn product-btn" id=${id} onclick="addToCart()">Add to Cart</button>
+                    </div>  
+                </div>`
+    })
+    .join("")
+
+
+    const lighterPerfumes = document.querySelector('.lighter-card-grid-wrapper')
+    products
+    .map((product) => {
+        const { name, id, price } = product
+        if ( product.category === "lighter")
+        lighterPerfumes.innerHTML += 
+
+            `<div class="card" >
+                    <div class="card-img-container" >
+                        <img src="images/venn.jpg" alt="" class="image">
+                    </div>
+                    <div class="card-subgrid">
+                        <h4 class="card-name">${name}</h4>
+                        <p>Eau du Parfum | 50mL </p>
+                        <p class="price-span">$${price}</p>
+                        <button class="card-btn product-btn" id=${id} onclick="addToCart()">Add to Cart</button>
+                    </div>  
+                </div>`
+    })
+    .join("")
+
+    const heavierPerfumes = document.querySelector('.heavier-card-grid-wrapper')
+    products
+    .map((product) => {
+        const { name, id, price } = product
+        if ( product.category === "heavier")
+        heavierPerfumes.innerHTML += 
 
             `<div class="card" >
                     <div class="card-img-container" >
@@ -523,41 +221,6 @@ function displayProducts(products) {
     .join("")
 }
 
-
-
-const lighterPerfumesCard = lighterPerfumes.map(
-    (product) => `<div class="card" >
-                    <div class="card-img-container" >
-                        <img src="images/venn.jpg" alt="" class="image">
-                    </div>
-                    <div class="card-subgrid">
-                        <h4 class="card-name">${product.name}</h4>
-                        <p>Eau du Parfum | 50mL </p>
-                        <p class="price-span">$${product.price}</p>
-                        <button class="card-btn product-btn" id=${product.id} onclick="addToCart()">Add to Cart</button>
-                    </div>  
-                </div>`
-)
-const lighterPerfumesDiv = document.querySelector('.lighter-card-grid-wrapper');
-lighterPerfumesDiv.innerHTML = lighterPerfumesCard.join("")
-
-
-
-const heavierPerfumesCard = heavierPerfumes.map(
-    (product) => `<div class="card" >
-                    <div class="card-img-container" >
-                        <img src="images/venn.jpg" alt="" class="image">
-                    </div>
-                    <div class="card-subgrid">
-                        <h4 class="card-name">${product.name}</h4>
-                        <p>Eau du Parfum | 50mL </p>
-                        <p class="price-span">$${product.price}</p>
-                        <button class="card-btn product-btn" id=${product.id} onclick="addToCart()">Add to Cart</button>
-                    </div>  
-                </div>`
-)
-const heavierPerfumesDiv = document.querySelector('.heavier-card-grid-wrapper');
-heavierPerfumesDiv.innerHTML = heavierPerfumesCard.join("")
 
 
 
